d808ba07ab13f28763568b85cee7813be30b5ce2
pokemogukunn <156386753+pokemogukunn@users.noreply.github.com>
2024-09-05 10:32:47 +0900

    Update 0.js

--- 0.js
+++ 0.js
@@ -1,11 +1,14 @@
-function checkPassword() {
+document.getElementById('passwordForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // フォームのデフォルトの送信を防ぐ
+
     const correctPassword = '85175';
     const enteredPassword = document.getElementById('password').value;
 
     if (enteredPassword === correctPassword) {
-        window.location.href = 'https://pokemogukunn.github.io/list.github.io/gamelist';
+        window.location.href = 'https://pokemogukunn.github.io/list.github.io/gamelist/';
     } else {
         window.location.href = 'https://www.google.co.jp';
     }
-}
+});
+
 <57>@pokemogukunn</57>
